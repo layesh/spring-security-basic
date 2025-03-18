e3e0bc2f9243ffd1708584e6975a911b922859e2
Layesh <layesh.iut@gmail.com>
2025-03-19 05:51:01 +0600

    my commit

---
+++ src/main/java/com/example/springsecuritybasic/SpringSecurityBasicApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Author:    Sample
+ * Created:   SampleDate
+ * (c) Copyright by SampleCompany.
+ * Adopted Originally from: https://spring.io/guides/gs/securing-web
+ **/
+
+package com.example.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicApplication.class, args);
+    }
+
+}
